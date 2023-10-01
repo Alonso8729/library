1d79102575a1875240223599abb53c27deb172ef
Alonso8729 <alon8729@gmail.com>
2023-10-01 12:28:42 +0300

    modified plain constructor to be inside of a class

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let myLibrary = [];
 
-function Book(name, author, pages, status) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+class Book {
+    constructor(name, author, pages, status) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
 }
 
 function addToLibrary(name, author, pages, status) {
